f6f043e5a60aab781b1a47fd27c03ef0dd63e141
Jairo Sanchez <jairosanchez@Jairos-MacBook-Air.local>
2021-03-23 13:11:29 -0700

    created enemies and their behaviors, also start menu

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import random
 
 clock = pygame.time.Clock()
 fps = 60
@@ -7,20 +8,92 @@ fps = 60
 screen_width = 800
 screen_height = 800
 
+Red = (255,0,0)
+Black = (0,0,0)
+
+enemy_x = 300
+enemy_y = 10
+
+enemy2_x = 100
+enemy2_y = 10
+
+exit_group = pygame.sprite.Group()
+
+
+
+
+speed = 10
+timer = 0
+time = 0
+
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Playformer")
+pygame.display.set_caption("Platformer")
 
 tile_size = 40
+main_menu = True
 
 #load images
 bg_img = pygame.image.load('img/forest.png')
+restart_img = pygame.image.load('img/restart_img.jpg')
+start_img = pygame.image.load('img/start_btn.png')
+exit_img = pygame.image.load('img/exit_btn.jpg')
 
+class Button():
+	def __init__(self, x, y, image):
+		self.image = image
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.clicked = False
+		
+	def draw(self):
+		action = False
+
+		#get mouse position
+		pos = pygame.mouse.get_pos()
+
+		#check mouseover and clicked conditions
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				action = True
+				self.clicked = True
+
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+
+
+		#draw button
+		screen.blit(self.image, self.rect)
+
+		return action
+
+def design(text, color, font_size, x, y):
+    font = pygame.font.SysFont(None, font_size)
+    img = font.render(text, True, color)
+    screen.blit(img, (x,y))
 
 def draw_grid():
 	for line in range(0, 20):
 		pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
 		pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
 
+class Exit(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		img = pygame.image.load('img/exit.png')
+		self.image = pygame.transform.scale(img, (tile_size, int(tile_size * 1.5)))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+
+def is_touched(x1, y1, x2, y2, width, height,):
+	#checks if the player has touched the enemy or something
+    if(x1 >= x2 + width) or (x2 >= x1 + width):
+         return False
+
+    if (y1 >= y2 + height) or (y2 >= y1 + height):
+         return False
+    return True
 
 class World():
 	def __init__(self, data):
@@ -48,6 +121,10 @@ class World():
 					img_rect.y = row_count * tile_size
 					tile = (img, img_rect)
 					self.tile_list.append(tile)
+				if tile == 8:
+					exit = Exit(col_count * tile_size, row_count * tile_size - (tile_size // 2))
+					exit_group.add(exit)
+
 				col_count += 1
 			row_count += 1
 
@@ -76,6 +153,7 @@ class Player():
 		self.vel_y = 0
 		self.jumped = False
 		self.direction = 0
+		self.in_air = True
 
 	def update(self):
 		dx = 0
@@ -84,7 +162,7 @@ class Player():
 
 		#get keypresses
 		key = pygame.key.get_pressed()
-		if key[pygame.K_SPACE] and self.jumped == False:
+		if key[pygame.K_SPACE] and self.jumped == False and self.in_air == False:
 			self.vel_y = -15
 			self.jumped = True
 		if key[pygame.K_SPACE] == False:
@@ -112,6 +190,7 @@ class Player():
 		dy += self.vel_y
 
 		#check for collision
+		self.in_air = True
 		for tile in world.tile_list:
 			#check for collision in x direction
 			if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
@@ -126,6 +205,7 @@ class Player():
 				elif self.vel_y >= 0:
 					dy = tile[1].top - self.rect.bottom
 					self.vel_y = 0
+					self.in_air = False
 
 
 
@@ -150,14 +230,14 @@ world_data = [
 [1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
 [1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1], 
-[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 2, 0, 0, 0, 1], 
 [1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
 [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
 [1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
 [1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1], 
-[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1], 
@@ -177,17 +257,69 @@ world = World(world_data)
 run = True
 
 player = Player(100, screen_height - 130)
+
+
 world = World(world_data)
+
+#creates button
+start_button = Button(screen_width // 5 - 250, screen_height // 2, start_img)
+exit_button = Button(screen_width // 2 + 30, screen_height // 2, exit_img)
 
 run = True
 while run:
+	
+	clock.tick(fps)
 
 	screen.blit(bg_img, (0, 0))
 
-	world.draw()
+	if main_menu == True:
+		if exit_button.draw():
+			run = False
+		if start_button.draw():
+			main_menu = False
 
-	player.update()
 
+	else:
+
+		world.draw()
+		exit_group.draw(screen)
+
+		player.update()
+		
+		enemy_y += speed
+		enemy2_y += speed
+
+		time += 1
+		if time >= 60:
+			time= 0 
+			timer += 1
+
+		
+		if enemy_y > screen_height:
+			enemy_y = 0
+			enemy_x = random.random() * (screen_width-player.width)
+
+		if enemy2_y > screen_height:
+			enemy2_y = 0
+			enemy2_x = random.random() * (screen_width-player.width)
+
+		if is_touched(player.rect.x , player.rect.y, enemy_x, enemy_y, player.width, player.height,):
+			points = 0
+			player.rect.x = 50
+			player.rect.y = 600
+
+		
+		if is_touched(player.rect.x , player.rect.y,  enemy2_x, enemy2_y, player.width, player.height,):
+			points = 0
+			player.rect.x = 50
+			player.rect.y = 600
+
+
+		
+
+		pygame.draw.rect(screen, Red, (enemy_x, enemy_y, player.height, player.width))
+		pygame.draw.rect(screen, Red, (enemy2_x, enemy2_y, player.height, player.width))
+		
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
